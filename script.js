e8db978b8cf8b7729f6e7d113892fdd952b22a23
Jan Erik <janerik.kodehode@gmail.com>
2024-11-13 14:59:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+Separate arrays
+1 for shopping list
+1 for recipe list
+
+const/let recipes = [
+{name: "spaghetti supreme",
+description: "First start with...",
+ingredient1: "spaghetti",
+ingredient2: "tomatoes",
+ingredient3: "olive oil"},]
+
+
+*/
+
+const recipesButton = document.querySelector("#recipies-button");
+const shoppingListButton = document.querySelector("shopping-list-button");
+const main = document.querySelector("#main");
+
+let recipies = [];
+let shoppingList = [];
