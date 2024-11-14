65c8ae6c2608d4d59b1a4498bb4c14323e3daaf4
Jan Erik <janerik.kodehode@gmail.com>
2024-11-14 14:20:10 +0100

    progress

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ ingredient3: "olive oil"},]
 
 */
 
-const recipesButton = document.querySelector("#recipies-button");
-const shoppingListButton = document.querySelector("shopping-list-button");
+const recipesButton = document.querySelector("#recipes-button");
+const shoppingListButton = document.querySelector("#shopping-list-button");
+const main = document.querySelector("#main");
 const list = document.querySelector("#list");
 const listAdd = document.querySelector("#list-add");
 const userInput = document.querySelector("#user-input");
+let displayPage = "recipe list";
 
 let recipes = [];
 let shoppingList = [];
@@ -32,21 +34,93 @@ listAdd.addEventListener("submit", (e) => {
   //   Clear input field after submitting
   userInput.value = "";
 
-  //   console.log(recipes);
   buildList();
 });
 
-const buildList = () => {
+// Clear list function
+const removeList = () => {
   while (list.firstChild) {
     list.firstChild.remove();
   }
+};
+
+const buildList = () => {
+  removeList();
   recipes.forEach((e, i) => {
     const name = document.createElement("input");
-    // name.classList.add("listElements");
-    name.value = e.name;
-    // console.log(e.name);
-    // console.log(name.value);
-    list.prepend(name);
-    // console.log(recipes);
+
+    if (displayPage === "recipe list") {
+      name.value = e.name;
+      list.prepend(name);
+    } else if (displayPage === "recipe") {
+      name.value = e.ingredient;
+      ingredients.append(ingredient);
+    }
+
+    name.addEventListener("click", () => {
+      console.log(`go to ${e.name} recipe`);
+      //   Some function or value
+      recipePage(e);
+    });
   });
+};
+
+recipesButton.addEventListener("click", () => {
+  removeList();
+  listAdd.style.display = "flex";
+});
+
+shoppingListButton.addEventListener("click", () => {
+  listAdd.style.display = "flex";
+  buildList();
+});
+
+const recipePage = (e) => {
+  const recipeArea = document.createElement("div");
+  const recipeName = document.createElement("h2");
+  const ingredients = document.createElement("div");
+  const ingredientsList = document.createElement("form");
+  const ingredientInput = document.createElement("input");
+  const addIngredientsButton = document.createElement("input");
+  const instructions = document.createElement("div");
+
+  recipeArea.id = "recipe-area";
+  ingredients.id = "ingredients";
+
+  ingredientsList.id = "ingredients-list";
+
+  ingredientInput.type = "text";
+  ingredientInput.name = "ingredient-input";
+  ingredientInput.id = "ingredient-input";
+
+  addIngredientsButton.type = "submit";
+  addIngredientsButton.value = "+";
+  addIngredientsButton.id = "add-ingredients-button";
+
+  removeList();
+  listAdd.style.display = "none";
+
+  recipeName.textContent = e.name;
+
+  console.log("abc");
+  //   Form event listener
+  ingredientsList.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const listData = new FormData(ingredientsList);
+
+    recipes.push({
+      ingredient: listData.get("ingredient-input"),
+    });
+    //   Clear input field after submitting
+    ingredientInput.value = "";
+    displayPage = "recipe";
+    console.log(recipes);
+    buildList();
+  });
+
+  addIngredientsButton.addEventListener("click", () => {});
+
+  ingredients.append("Ingredients", ingredientInput, addIngredientsButton);
+  recipeArea.append(recipeName, ingredients, instructions);
+  main.append(recipeArea);
 };
