3967e904a58732a7cd9d7af1569186fd0fc69c9b
Jan Erik <janerik.kodehode@gmail.com>
2024-11-14 10:00:07 +0100

    progress

--- script.js
+++ script.js
@@ -15,7 +15,38 @@ ingredient3: "olive oil"},]
 
 const recipesButton = document.querySelector("#recipies-button");
 const shoppingListButton = document.querySelector("shopping-list-button");
-const main = document.querySelector("#main");
+const list = document.querySelector("#list");
+const listAdd = document.querySelector("#list-add");
+const userInput = document.querySelector("#user-input");
 
-let recipies = [];
+let recipes = [];
 let shoppingList = [];
+
+listAdd.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const listData = new FormData(listAdd);
+
+  recipes.push({
+    name: listData.get("user-input"),
+  });
+  //   Clear input field after submitting
+  userInput.value = "";
+
+  //   console.log(recipes);
+  buildList();
+});
+
+const buildList = () => {
+  while (list.firstChild) {
+    list.firstChild.remove();
+  }
+  recipes.forEach((e, i) => {
+    const name = document.createElement("input");
+    // name.classList.add("listElements");
+    name.value = e.name;
+    // console.log(e.name);
+    // console.log(name.value);
+    list.prepend(name);
+    // console.log(recipes);
+  });
+};
