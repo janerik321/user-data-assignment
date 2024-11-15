07aab9cd70d64ddd2a2d6d1e2302402bc0d27e45
Jan Erik <janerik.kodehode@gmail.com>
2024-11-15 14:38:56 +0100

    progress

--- script.js
+++ script.js
@@ -13,114 +13,132 @@ ingredient3: "olive oil"},]
 
 */
 
+// Header
 const recipesButton = document.querySelector("#recipes-button");
 const shoppingListButton = document.querySelector("#shopping-list-button");
+
 const main = document.querySelector("#main");
-const list = document.querySelector("#list");
-const listAdd = document.querySelector("#list-add");
-const userInput = document.querySelector("#user-input");
-let displayPage = "recipe list";
 
-let recipes = [];
-let shoppingList = [];
+// Shopping list page
+const shoppingListPage = document.querySelector("#shopping-list-page");
+const shoppingListAdd = document.querySelector("#shopping-list-add");
+const shoppingListInput = document.querySelector("#shopping-list-input");
+const shoppingList = document.querySelector("#shopping-list");
 
-listAdd.addEventListener("submit", (e) => {
+// Recipes list page
+const recipeListPage = document.querySelector("#recipes-list-page");
+const recipeListAdd = document.querySelector("#recipe-list-add");
+const recipeListInput = document.querySelector("#recipe-list-input");
+const recipeList = document.querySelector("#recipe-list");
+
+// Recipes page
+const recipePage = document.querySelector("#recipe-page");
+const recipeName = document.querySelector("#recipe-name");
+const ingredientAdd = document.querySelector("#ingredient-add");
+const ingredientInput = document.querySelector("#ingredient-input");
+const ingredientList = document.querySelector("#ingredient-list");
+
+let recipesArray = [];
+let shoppingListArray = [];
+
+let displayPage = "shopping list";
+let activeRecipe = 0;
+
+// shoppingListPage.style.display = "none";
+
+// Shopping list page
+shoppingListAdd.addEventListener("submit", (e) => {
   e.preventDefault();
-  const listData = new FormData(listAdd);
+  const listData = new FormData(shoppingListAdd);
 
-  recipes.push({
-    name: listData.get("user-input"),
+  shoppingListArray.push({
+    item: listData.get("shopping-list-input"),
   });
-  //   Clear input field after submitting
-  userInput.value = "";
 
+  shoppingListInput.value = "";
+  displayPage = "shopping list";
   buildList();
+});
+
+// Recipe list page
+recipeListAdd.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const listData = new FormData(recipeListAdd);
+
+  recipesArray.push({
+    name: listData.get("recipe-list-input"),
+  });
+
+  recipeListInput.value = "";
+  displayPage = "recipe list";
+  buildList();
+});
+
+// Recipe page - ingredients
+ingredientAdd.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(activeRecipe);
+  const listData = new FormData(ingredientAdd);
+  if (!recipesArray[activeRecipe].ingredients) {
+    recipesArray[activeRecipe].ingredients = [];
+  }
+  recipesArray[activeRecipe].ingredients.push("pasta");
+  console.log(recipesArray);
+  displayPage = "recipe";
+  buildList();
+  //   console.log(displayPage);
 });
 
 // Clear list function
 const removeList = () => {
-  while (list.firstChild) {
-    list.firstChild.remove();
+  while (shoppingList.firstChild) {
+    shoppingList.firstChild.remove();
+  }
+  while (recipeList.firstChild) {
+    recipeList.firstChild.remove();
   }
 };
 
 const buildList = () => {
   removeList();
-  recipes.forEach((e, i) => {
-    const name = document.createElement("input");
-
-    if (displayPage === "recipe list") {
-      name.value = e.name;
-      list.prepend(name);
-    } else if (displayPage === "recipe") {
-      name.value = e.ingredient;
-      ingredients.append(ingredient);
-    }
-
-    name.addEventListener("click", () => {
-      console.log(`go to ${e.name} recipe`);
-      //   Some function or value
-      recipePage(e);
+  if (displayPage === "shopping list") {
+    shoppingListArray.forEach((e, i) => {
+      const item = document.createElement("input");
+      item.value = e.item;
+      shoppingList.prepend(item);
     });
-  });
+  } else if (displayPage === "recipe list") {
+    recipesArray.forEach((e, i) => {
+      const name = document.createElement("input");
+
+      name.value = e.name;
+      recipeList.prepend(name);
+
+      name.addEventListener("click", () => {
+        console.log(`go to ${e.name} recipe`);
+        activeRecipe = i;
+        // displayPage = "recipe";
+        // console.log(activeRecipe);
+        console.log(displayPage);
+      });
+    });
+  } else if (displayPage === "recipe") {
+    recipesArray.forEach((e, i) => {
+      const ingredient = document.createElement("input");
+      //   ingredient.value = e.ingredients;
+      ingredient.value = e.ingredients...
+      ingredientList.prepend(ingredient);
+      console.log(e);
+    });
+  }
 };
 
 recipesButton.addEventListener("click", () => {
   removeList();
-  listAdd.style.display = "flex";
+  recipeListAdd.style.display = "flex";
 });
 
 shoppingListButton.addEventListener("click", () => {
-  listAdd.style.display = "flex";
+  recipeListAdd.style.display = "flex";
   buildList();
 });
-
-const recipePage = (e) => {
-  const recipeArea = document.createElement("div");
-  const recipeName = document.createElement("h2");
-  const ingredients = document.createElement("div");
-  const ingredientsList = document.createElement("form");
-  const ingredientInput = document.createElement("input");
-  const addIngredientsButton = document.createElement("input");
-  const instructions = document.createElement("div");
-
-  recipeArea.id = "recipe-area";
-  ingredients.id = "ingredients";
-
-  ingredientsList.id = "ingredients-list";
-
-  ingredientInput.type = "text";
-  ingredientInput.name = "ingredient-input";
-  ingredientInput.id = "ingredient-input";
-
-  addIngredientsButton.type = "submit";
-  addIngredientsButton.value = "+";
-  addIngredientsButton.id = "add-ingredients-button";
-
-  removeList();
-  listAdd.style.display = "none";
-
-  recipeName.textContent = e.name;
-
-  console.log("abc");
-  //   Form event listener
-  ingredientsList.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const listData = new FormData(ingredientsList);
-
-    recipes.push({
-      ingredient: listData.get("ingredient-input"),
-    });
-    //   Clear input field after submitting
-    ingredientInput.value = "";
-    displayPage = "recipe";
-    console.log(recipes);
-    buildList();
-  });
-
-  addIngredientsButton.addEventListener("click", () => {});
-
-  ingredients.append("Ingredients", ingredientInput, addIngredientsButton);
-  recipeArea.append(recipeName, ingredients, instructions);
-  main.append(recipeArea);
-};
